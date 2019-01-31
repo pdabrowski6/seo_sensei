5e66e08fd6acddf9b53d02409583ddbecc9dde99
Paweł Dąbrowski <dziamber@gmail.com>
2019-01-31 15:55:08 +0100

    Add default generator

---
+++ lib/generators/seo_sensei/generate_translations_generator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails/generators/base'
+
+module SeoSensei
+  module Generators
+    class GenerateTranslationsGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc ""
+    end
+  end
+end
