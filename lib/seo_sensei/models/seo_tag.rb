453eade34103c36b712a894e21877c06da0238d8
Paweł Dąbrowski <dziamber@gmail.com>
2019-05-03 14:08:34 +0200

    Allow to store image

--- lib/seo_sensei/models/seo_tag.rb
+++ lib/seo_sensei/models/seo_tag.rb
@@ -2,6 +2,7 @@ module SeoSensei
   module Models
     class SeoTag < ::ActiveRecord::Base
       belongs_to :seoable, polymorphic: true
+      has_one_attached :image
 
       def to_h
         slice("title", "keywords", "description")
